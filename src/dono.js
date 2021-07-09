c5165d21e72fc15cbcdede0da7315542b41c66a5
KANTDEV71 <kantmods71@gmail.com>
2021-07-09 04:26:26 -0300

    First commit

---
+++ src/dono.js
@@ -0,0 +1,15 @@
+const dono = (prefix, sender) => {
+  return`
+
+╔══✪〘 DONO 〙✪════
+║
+╠➥NOME: VITOR
+╠➥NICK: 𝐊𝐀𝐍𝐓🐊
+╠➥SEXO: MASCULINO
+╠➥NUMERO: wa.me/5571982507697
+╠➥AMORES:UE? KKKK VEJA NA MINHA GIT MEU DONO ESTA TRISTE
+╠➥INSTA: SEM NO MOMENTO
+╚═✪〘 𝐊𝐀𝐍𝐓🐊 〙✪══`
+
+}
+exports.dono = dono
---
+++ src/putalx.js
@@ -0,0 +1,22 @@
+const putalx = (pushname,prefix) => {
+return`
+
+ETA FAFADINHO ENTÃO TU QUER CONTATINHOS +18 NE HEHEHE
+ENTÃO AQUI ESTA AMIGO VARIOS CONTRATOS DE PUTAS DE LUXO
+
+PRODUZIDO BY 𝐊𝐀𝐍𝐓🐊
+
+𝐊𝐀𝐍𝐓🐊
+
+𝐊𝐀𝐍𝐓🐊 NO TOPO FAMÍLIA 👑😈
+
+   〘 𝐊𝐀𝐍𝐓🐊 〙!
+    ║▌│█║▌│ █║▌│█│║▌║
+    ║▌│█║▌│ █║▌│█│║▌║
+
+➣ Copyright © 𝐊𝐀𝐍𝐓🐊愛 2021`
+}
+
+
+
+exports.putalx = putalx
